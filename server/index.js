ade633503520eb3d17b760606a78d878018ff15a
John Almazan <almazankaze@live.com>
2022-03-13 19:24:07 -0500

    added initial files

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const PORT = process.env.PORT || 5000;
