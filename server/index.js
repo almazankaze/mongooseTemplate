e2d4543b1c467c5ca0adeaa21aa2a4c69eb2c408
John Almazan <almazankaze@live.com>
2022-03-13 19:37:23 -0500

    added initial files

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.CONNECT, {
+  .connect(process.env.CONNECTION, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
