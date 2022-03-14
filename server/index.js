56aefd92dbc46675d68f48dfd414d7b065754100
John Almazan <almazankaze@live.com>
2022-03-13 19:32:07 -0500

    added initial files

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import { Console } from "console";
 
 const app = express();
 dotenv.config();
@@ -11,3 +12,17 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((e) =>
+    console.log("Something went wrong and could not start server.")
+  );
