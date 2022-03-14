f3f00ed1ef427b1302dbaa2a0ef8b6ed7926751d
John Almazan <almazankaze@live.com>
2022-03-13 19:09:11 -0500

    added initial files

--- server/index.js
+++ server/index.js
@@ -3,3 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 
 const app = express();
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
