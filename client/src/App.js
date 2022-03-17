6701f1bc3446ee58502a2aedc06349c63e2693f2
John Almazan <almazankaze@live.com>
2022-03-16 21:41:51 -0500

    added form component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Form from "./components/form/Form";
 import PostsContainer from "./components/posts/PostsContainer";
 
 const App = () => {
   return (
     <div>
+      <Form />
       <PostsContainer />
     </div>
   );
--- client/src/components/form/Form.js
+++ client/src/components/form/Form.js
@@ -1,10 +1,35 @@
 import React from "react";
+import "./form.css";
 
 const Form = () => {
   return (
-    <>
-      <h1>form</h1>
-    </>
+    <div className="form-container">
+      <div className="post-form">
+        <form autocomplete="off">
+          <div className="input-container">
+            <input
+              type="text"
+              placeholder="First Name"
+              className="post-input"
+            />
+            <span className="input-error">Need a first name</span>
+          </div>
+          <div className="input-container">
+            <input type="text" placeholder="Last Name" className="post-input" />
+            <span className="input-error">Need a last name</span>
+          </div>
+
+          <div className="input-container">
+            <input type="text" placeholder="Message" className="post-input" />
+            <span className="input-error">Enter a message</span>
+          </div>
+
+          <button className="btn form-btn" type="button">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
   );
 };
 
